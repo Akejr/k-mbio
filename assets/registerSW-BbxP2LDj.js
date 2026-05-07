894fcb50f69635f2741c522a5178c196689555a6
Ecasanovs <contato@evandrocasanova.com>
2026-05-07 16:50:50 -0300

    deploy: 2026-05-07T19:50:50.283Z

---
+++ assets/registerSW-BbxP2LDj.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/virtual_pwa-register-DfYQtTwK.js","assets/index-DM8IpvY3.js","assets/index-Rb_EQfsv.css"])))=>i.map(i=>d[i]);
+import{t as e}from"./index-DM8IpvY3.js";async function t(){if(!(typeof window>`u`||!(`serviceWorker`in navigator)))try{let t=(await e(()=>import(`./virtual_pwa-register-DfYQtTwK.js`),__vite__mapDeps([0,1,2]))).registerSW;if(typeof t!=`function`)return;t({immediate:!0,onRegisterError(e){console.warn(`[pwa] falha ao registrar Service Worker:`,e)}})}catch(e){console.warn(`[pwa] Service Worker não registrado:`,e)}}export{t as registerServiceWorker};
+//# sourceMappingURL=registerSW-BbxP2LDj.js.map
