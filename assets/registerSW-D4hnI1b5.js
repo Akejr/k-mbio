371b17fa99ab67ea67f3b6d0245f302d006fa4d6
Ecasanovs <contato@evandrocasanova.com>
2026-05-27 13:00:54 -0300

    deploy: 2026-05-27T16:00:54.170Z

---
+++ assets/registerSW-D4hnI1b5.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/virtual_pwa-register-CiJCB6ek.js","assets/index-BaIc_uon.js","assets/index-bzcGAiGm.css"])))=>i.map(i=>d[i]);
+import{t as e}from"./index-BaIc_uon.js";async function t(){if(!(typeof window>`u`||!(`serviceWorker`in navigator)))try{let t=(await e(()=>import(`./virtual_pwa-register-CiJCB6ek.js`),__vite__mapDeps([0,1,2]))).registerSW;if(typeof t!=`function`)return;t({immediate:!0,onRegisterError(e){console.warn(`[pwa] falha ao registrar Service Worker:`,e)}})}catch(e){console.warn(`[pwa] Service Worker não registrado:`,e)}}export{t as registerServiceWorker};
+//# sourceMappingURL=registerSW-D4hnI1b5.js.map
